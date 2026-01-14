a3869b0cb802256a042a2a8f07d61de187b983ae
Dennis Amematekpor Anane <dennisamematekpor@gmail.com>
2026-01-14 10:50:53 -0500

    SquareToCircle

--- main.py
+++ main.py
@@ -203,3 +203,15 @@ class SquareAndCircle(Scene):
 
         self.play(Create(group))
         self.wait()
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
